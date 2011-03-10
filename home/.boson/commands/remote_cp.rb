f53646df274681a6a4d4550a576521276b120858
Martinos <chabotm@gmail.com>
2011-03-10 15:47:52 -0500

    WIP gzip remote copy.

--- home/.boson/commands/remote_cp.rb
+++ home/.boson/commands/remote_cp.rb
@@ -1,26 +1,45 @@
 module RemoteCp
+  def self.included(base)
+    require 'zlib'
+    require 'archive/tar/minitar'
+    include Archive::Tar
+  end
 
   def rmcp(filename = nil)
-    if filename == nil
-      basename = "anonymous"
-      content = $stdin.read
+    full_path = File.expand_path(filename)
+    basename = File.basename(full_path)
+    type = nil
+    content_type = nil 
+    content = if File.directory?(full_path)
+      content_type = "application/gzip"
+      io = StringIO.new("", "rb+")
+      tgz = Zlib::GzipWriter.new(io)
+      Minitar.pack(full_path, tgz)
+      basename = "zipped_directory"
+      type = 'directory' 
+      io.rewind
+      io.read
     else
-      full_path = File.expand_path(filename)
-      basename = File.basename(full_path)
-
-      content = File.open(full_path)
+      type = 'file'
+      if filename == nil
+        basename = "anonymous"
+        $stdin.read
+      else
+        File.open(full_path)
+      end
     end
     s3_connect
 
-
     S3Object.store("file_name.txt", basename, 'RemoteClipboard')
-    S3Object.store("content", content, 'RemoteClipboard')
+    S3Object.store("content", content, 'RemoteClipboard', :content_type => content_type)
+    S3Object.store("type", type, 'RemoteClipboard')
   end
 
   def rmp
     s3_connect
 
     file_name = S3Object.find("file_name.txt", 'RemoteClipboard').value
+    type = S3Object.find("type", 'RemoteClipboard').value
 
     if File.exist?(file_name)
       puts "#{file_name} already exist."
@@ -28,10 +47,16 @@ module RemoteCp
       res = $stdin.gets.chomp
       return unless res == "y"
     end
-
+     
     content = S3Object.find('content', 'RemoteClipboard').value
-    File.open(file_name, "w+") {|f| f << content}
-    puts "#{file_name} copied."
+    case type
+    when 'file'
+      File.open(file_name, "w+") {|f| f << content}
+      puts "#{file_name} copied."
+    when 'directory'
+      tgz = Zlib::GzipReader.new(StringIO.new(content))
+      Minitar.unpack(tgz, file_name)
+    end
   end
 
   def rmfn
@@ -42,6 +67,9 @@ module RemoteCp
     rm_content
   end
 
+  def my_command
+    "http://www.google.com"
+  end
 private
   def s3_connect
     require 'aws/s3'
