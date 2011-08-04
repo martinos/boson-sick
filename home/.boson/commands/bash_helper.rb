f73ed0bc24023e030de640d582b19c832a5be1bb
Martinos <chabotm@gmail.com>
2011-08-04 15:55:55 -0400

    Adding bash_helper

--- home/.boson/commands/bash_helper.rb
+++ home/.boson/commands/bash_helper.rb
@@ -1,8 +1,8 @@
 module BashHelper 
-  def preprocess(preprocessor_command, command)
+  def preprocess(preprocessor_command, *command)
     while filename = $stdin.gets
       filename.chomp!
-      cmd = "#{preprocessor_command} #{filename} | #{command}"
+      cmd = "#{preprocessor_command} #{filename} | #{command.join(' ')}"
       IO.popen(cmd, "r").each_line do |line|
         print "#{filename}: #{line}" 
       end
