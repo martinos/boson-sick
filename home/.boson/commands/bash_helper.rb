faa87374801e1f610631f320c6ad2b32b195dfd5
Martinos <chabotm@gmail.com>
2011-08-04 15:14:20 -0400

    BUGFIX

--- home/.boson/commands/bash_helper.rb
+++ home/.boson/commands/bash_helper.rb
@@ -2,7 +2,7 @@ module BashHelper
   def preprocess(preprocessor_command, command)
     while filename = $stdin.gets
       filename.chomp!
-      cmd = "#{preprocessor_command} #{filename} | #{command} #{filename}"
+      cmd = "#{preprocessor_command} #{filename} | #{command}"
       IO.popen(cmd, "r").each_line do |line|
         print "#{filename}: #{line}" 
       end
