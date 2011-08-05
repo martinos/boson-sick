45046dd67aeca2090459afd165d099054ea8b975
Martinos <chabotm@gmail.com>
2011-08-05 10:57:02 -0400

    Improving deployment

--- home/.boson/commands/rails/deploy.rb
+++ home/.boson/commands/rails/deploy.rb
@@ -4,9 +4,15 @@ module Deploy
   end
 
   def deploy
-    system 'sudo git pull'
-    system 'sudo git submodule update'
-    system 'bundle install --without=test'
-    restart_server
+    system 'sudo git fetch'
+    system 'sudo git diff remotes/origin/master'
+    print "Do you want to continue the deployement?"
+    
+    if STDIN.gets.chomp == 'y'
+      system 'sudo git merge remotes/origin/master'
+      system 'sudo git submodule update'
+      system 'bundle install --without=test'
+      restart_server
+    end
   end
 end
